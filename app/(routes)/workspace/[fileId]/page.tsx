8e727d50517d052ad9c8b477eb07979d7e5c03e4
impruthvi <pruthvirajput97@gmail.com>
2024-10-07 18:30:56 +0530

    Add EditorJS dependencies and update file handling

--- app/(routes)/workspace/[fileId]/page.tsx
+++ app/(routes)/workspace/[fileId]/page.tsx
@@ -1,24 +1,48 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import WorkspaceHeader from "../_components/WorkspaceHeader";
+import Editor from "../_components/Editor";
+import { useConvex } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { FILE } from "../../dashboard/_components/FileList";
 
-type Props = {};
-
-const Workspace = (props: Props) => {
+function Workspace({ params }: any) {
   const [triggerSave, setTriggerSave] = useState(false);
+  const convex = useConvex();
+  const [fileData, setFileData] = useState<FILE | any>();
+  useEffect(() => {
+    console.log("FILEID", params.fileId);
+    params.fileId && getFileData();
+  }, []);
 
+  const getFileData = async () => {
+    const result = await convex.query(api.files.getFileById, {
+      _id: params.fileId,
+    });
+    setFileData(result);
+  };
   return (
     <div>
       <WorkspaceHeader onSave={() => setTriggerSave(!triggerSave)} />
-      {/* Workspace Layout */}
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        {/* Document */}
-        <div className="bg-blue-400 h-screen">Document</div>
-        {/* Whiteboard/canvas */}
-        <div className="bg-red-400 h-screen">Canvas</div>
+
+      {/* Workspace Layout  */}
+      <div
+        className="grid grid-cols-1
+      md:grid-cols-2"
+      >
+        {/* Document  */}
+        <div className=" h-screen">
+          <Editor
+            onSaveTrigger={triggerSave}
+            fileId={params.fileId}
+            fileData={fileData}
+          />
+        </div>
+        {/* Whiteboard/canvas  */}
+        <div className=" h-screen border-l">Canvas</div>
       </div>
     </div>
   );
-};
+}
 
 export default Workspace;
