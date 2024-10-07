8120f71a0e5889f80d97476b14b28db4ec41331d
impruthvi <pruthvirajput97@gmail.com>
2024-10-07 13:12:52 +0530

    Add image domains and new dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    images: {
+        domains: ['localhost', 'gravatar.com', 'lh3.googleusercontent.com'],
+    }
 };
 
 export default nextConfig;
