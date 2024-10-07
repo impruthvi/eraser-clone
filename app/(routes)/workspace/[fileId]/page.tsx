ed79883021270a42c81a0677a9759df19c69212a
impruthvi <pruthvirajput97@gmail.com>
2024-10-07 17:08:01 +0530

    Add workspace layout

--- app/(routes)/workspace/[fileId]/page.tsx
+++ app/(routes)/workspace/[fileId]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import WorkspaceHeader from "../_components/WorkspaceHeader";
 
@@ -10,7 +10,13 @@ const Workspace = (props: Props) => {
   return (
     <div>
       <WorkspaceHeader onSave={() => setTriggerSave(!triggerSave)} />
-      Workspace
+      {/* Workspace Layout */}
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        {/* Document */}
+        <div className="bg-blue-400 h-screen">Document</div>
+        {/* Whiteboard/canvas */}
+        <div className="bg-red-400 h-screen">Canvas</div>
+      </div>
     </div>
   );
 };
